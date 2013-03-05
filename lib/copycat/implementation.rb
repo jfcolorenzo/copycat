35daa050de1aa7a9749f2feb26cc9af4470fc2fc
Juan González <juan@zorros.be>
2013-03-05 14:04:25 +0100

    Suport for scoped translations (as used by simple_form)

--- lib/copycat/implementation.rb
+++ lib/copycat/implementation.rb
@@ -3,11 +3,15 @@ module Copycat
     # this method overrides part of the i18n gem, lib/i18n/backend/simple.rb
     def lookup(locale, key, scope = [], options = {})
       return super unless ActiveRecord::Base.connected? && CopycatTranslation.table_exists?
-      cct = CopycatTranslation.where(locale: locale.to_s, key: key.to_s).first
+
+      scoped_key = I18n.normalize_keys(nil, key, scope, options[:separator]).join(".")
+
+      cct = CopycatTranslation.where(locale: locale.to_s, key: scoped_key).first
       return cct.value if cct
+
       value = super(locale, key, scope, options)
       if value.is_a?(String) || value.nil?
-        CopycatTranslation.create(locale: locale.to_s, key: key.to_s, value: value)
+        CopycatTranslation.create(locale: locale.to_s, key: scoped_key, value: value)
       end
       value
     end
--- spec/lib/copycat_spec.rb
+++ spec/lib/copycat_spec.rb
@@ -29,15 +29,22 @@ describe Copycat do
     it "returns simple lookup if copycat_translation missing" do
       base.lookup(nil, '').should == "translation missing"
     end
+
     it "returns copycat_translation if present" do
       cct = FactoryGirl.create(:copycat_translation)
       base.lookup(cct.locale, cct.key).should == cct.value
     end
+
     it "creates copycat_translation if one is missing" do
       CopycatTranslation.where(locale: 'en', key: 'foo').should be_empty
       base.lookup('en', 'foo')
       CopycatTranslation.where(locale: 'en', key: 'foo').should_not be_empty
     end
-  end
 
+    it "creates scoped copycat_translation if one is missing" do
+      CopycatTranslation.where(locale: 'en', key: 'the_scope.foo').should be_empty
+      base.lookup('en', 'foo', :"the_scope")
+      CopycatTranslation.where(locale: 'en', key: 'the_scope.foo').should_not be_empty
+    end
+  end
 end
