d2dad8436cbeb378306016c6f336ba533a2a190c
jospergon <josemanuelprz@gmail.com>
2017-05-17 14:03:56 +0200

    - Fixed adding new translations keys when using Rails cache.

--- lib/copycat/implementation.rb
+++ lib/copycat/implementation.rb
@@ -6,12 +6,16 @@ module Copycat
 
       scoped_key = I18n.normalize_keys(nil, key, scope, options[:separator]).join(".")
 
-      cct = if Copycat.use_cache
-        Rails.cache.fetch("_cc_#{locale.to_s}_#{scoped_key}") do
+      cct = nil
+
+      if Copycat.use_cache
+        cct = Rails.cache.fetch("_cc_#{locale.to_s}_#{scoped_key}") do
           CopycatTranslation.where(locale: locale.to_s, key: scoped_key).first
         end
-      else
-        CopycatTranslation.where(locale: locale.to_s, key: scoped_key).first
+      end
+
+      if cct.nil?
+        cct = CopycatTranslation.where(locale: locale.to_s, key: scoped_key).first
       end
 
       return cct.value if cct
